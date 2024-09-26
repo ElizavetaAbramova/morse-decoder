baa7da1e690e6ddeb252922fd24a2d1d2878272d
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-09-26 21:05:46 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let result = '';
+    let morseWord = '';
+    let decodedWord = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        if (expr[i] === '*') {
+            result = result + ' ';
+        } else {
+            for (let j = i; j < i + 9; j += 2) {
+                if (expr[j] === '1' && expr[j + 1] === '0') {
+                    morseWord = morseWord + '.';
+                } else if (expr[j] === '1' && expr[j + 1] === '1'){
+                    morseWord = morseWord + '-';
+                }
+            }
+            decodedWord = `${MORSE_TABLE[morseWord]}`;
+            result = result + decodedWord;
+            morseWord = '';
+        }
+
+
+
+    }
+    // console.log(result);
+
+    return result;
 }
 
 module.exports = {
